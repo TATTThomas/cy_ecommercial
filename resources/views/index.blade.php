9c6754acce9fd64cda701bef7aa9200a71fae46f
Jeter Chan <clocklife0226@gmail.com>
2024-02-20 23:49:43 +0800

    feat:item_detail page 切版 & update apapp.css & add layouts folder and item folder

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,11 +17,11 @@
   <meta name="keywords" content="bootstrap, bootstrap4" />
 
 		<!-- Bootstrap CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-		<link href="css/tiny-slider.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
-		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
+		<link href="{{ asset('css/tiny-slider.css') }}" rel="stylesheet">
+		<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+		<title>駿英企業社 - @yield('title')</title>
 	</head>
 
 	<body>
@@ -136,22 +136,7 @@
 			</div>
 		</div>
 		<!-- End Product Section -->
-
-		<!-- Start Why Choose Us Section -->
-		<!-- End Why Choose Us Section -->
-
-		<!-- Start We Help Section -->
-		<!-- End We Help Section -->
-
-		<!-- Start Popular Product -->
-		<!-- End Popular Product -->
-
-		<!-- Start Testimonial Slider -->
-		<!-- End Testimonial Slider -->
-
-		<!-- Start Blog Section -->
-		<!-- End Blog Section -->	
-
+		
 		<!-- Start Footer Section -->
 		<footer class="footer-section">
 			<div class="container relative">
